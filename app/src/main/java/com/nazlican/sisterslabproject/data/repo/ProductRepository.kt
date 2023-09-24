a9c401192b9cf712d3293e4355549c843b9e381b
Nazlıcan <nazlicanterliksiz@gmail.com>
2023-09-24 16:05:38 +0300

    add repository

--- app/src/main/java/com/nazlican/sisterslabproject/data/repo/ProductRepository.kt
+++ app/src/main/java/com/nazlican/sisterslabproject/data/repo/ProductRepository.kt
@@ -1,12 +1,16 @@
 package com.nazlican.sisterslabproject.data.repo
 
+import com.nazlican.sisterslabproject.data.model.DetailRequest
 import com.nazlican.sisterslabproject.data.model.Product
 import com.nazlican.sisterslabproject.di.RetrofitModule
 import retrofit2.Response
+import retrofit2.http.Query
 
 class ProductRepository {
 
     val retrofitModule = RetrofitModule()
 
     suspend fun getProduct(): Response<Product> = retrofitModule.retrofit.getProduct()
+
+    suspend fun detailProduct(@Query("id") id: Int): Response<DetailRequest> =  retrofitModule.retrofit.detailProduct(id)
 }
--- app/src/main/java/com/nazlican/sisterslabproject/ui/detail/DetailFragmentViewModel.kt
+++ app/src/main/java/com/nazlican/sisterslabproject/ui/detail/DetailFragmentViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.nazlican.sisterslabproject.data.model.ProductXX
+import com.nazlican.sisterslabproject.data.repo.ProductRepository
 import com.nazlican.sisterslabproject.di.RetrofitModule
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -16,13 +17,14 @@ class DetailFragmentViewModel : ViewModel(){
 
     val detailLiveData = MutableLiveData<ProductXX?>()
     val retrofitModule = RetrofitModule()
+    val productRepository = ProductRepository()
 
 
     fun detailProducts(id:Int) {
         job = CoroutineScope(Dispatchers.IO).launch {
-            val response = retrofitModule.retrofit.detailProduct(id)
-            if (response.isSuccessful) {
-                response.body()?.let {products ->
+            val result =productRepository.detailProduct(id)
+            if (result.isSuccessful) {
+                result.body()?.let {products ->
                  Log.d("if içi", products.toString())
                     detailLiveData.postValue(products.product)
                 }
