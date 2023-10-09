3d8e2e27a3b0de2301aecabd034da4f9b74e4ef3
Nazlıcan <nazlicanterliksiz@gmail.com>
2023-10-09 17:15:29 +0300

    update design

--- app/src/main/java/com/nazlican/sisterslabproject/util/DownloadImage.kt
+++ app/src/main/java/com/nazlican/sisterslabproject/util/DownloadImage.kt
@@ -2,7 +2,6 @@ package com.nazlican.sisterslabproject.util
 
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
-
 fun ImageView.downloadFromUrl(url: String) {
     Picasso.get()
         .load(url)
